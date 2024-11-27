31eb035ffc50160d594f0d6eef9ac5f5a4719dcd
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-11-26 22:42:04 -0300

    Se agrego menu hamburgueza en Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import "../../../mediaqueries.css";
+
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav id="desktop-nav">
@@ -22,6 +31,32 @@ const Header = () => {
             </li>
             <li>
               <button class="button-primary">Ingresar</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav id="hamburger-nav">
+        <div className="logo">Fonseca </div>
+        <div className="hamburger-menu">
+          <div className="hamburger-icon" onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          {/* Conditional class to show/hide the menu */}
+          <ul className={`menu-links ${isMenuOpen ? "open" : ""}`}>
+            <li>
+              <a href="#about">Sobre mí</a>
+            </li>
+            <li>
+              <a href="#experience">Experiencia</a>
+            </li>
+            <li>
+              <a href="#projects">Proyectos</a>
+            </li>
+            <li>
+              <a href="#contact">Contacto</a>
             </li>
           </ul>
         </div>
