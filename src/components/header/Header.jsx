fd6e201482adc81908b6a22db46e71065c4eb83d
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-11-26 21:51:29 -0300

    Se ha creado componente Header.jsx

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <nav id="desktop-nav">
+        <div class="logo">Fonseca Claudio</div>
+        <div>
+          <ul class="nav-links">
+            <li>
+              <a href="#about">Sobre Mí</a>
+            </li>
+            <li>
+              <a href="#experience">Experiencia</a>
+            </li>
+            <li>
+              <a href="#projects">Proyectos</a>
+            </li>
+            <li>
+              <a href="#contact">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
